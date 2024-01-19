4952996affd3cffa628702fada20d9a4d9b8fb50
Petr Kostelník <kostepe2@fit.cvut.cz>
2024-01-19 18:04:45 +0100

    prototype ball implementation

--- src/my-game.ts
+++ src/my-game.ts
@@ -1,9 +1,113 @@
 import * as ECS from '../libs/pixi-ecs';
+import * as PIXI from 'pixi.js';
 
 const SCENE_WIDTH = 800;
 const SCENE_HEIGHT = 600;
 const RESOLUTION = 1;
 const BACK_GROUND_COLOR = 0xFFFFFFF;
+const GRAVITY = 0.05;
+const PLAYER_VERTICAL_SPEED = 18;
+const PLAYER_HORIZONTAL_SPEED = 0.8;
+const SLIDING = 0.9;
+
+type Vec = {
+	x: number;
+	y: number;
+}
+
+enum BallMoveStates {
+	STAND = 'STAND',
+	JUMP = 'JUMP',
+	FALL = 'FALL',
+}
+
+enum PlayerActions {
+	RIGHT = 1,
+	LEFT = -1,
+}
+
+enum Tags{
+	BALL = 'BALL',
+	COLORING = 'COLORING'
+}
+
+class BallController extends ECS.Component {
+	speed: Vec = { x: 0, y: 0 };
+	gravity: number = GRAVITY;
+	ballMoveState: BallMoveStates = BallMoveStates.STAND
+
+	onInit(){
+		this.ballMoveState = BallMoveStates.STAND;
+	}
+	onUpdate(delta: number, absolute: number) {
+		this.updateHorizontalMove(delta);
+		this.updateVerticalMove(delta);
+		this.checkCollisions();
+		this.applyDynamics(delta);
+	}
+
+	updateVerticalMove(delta: number){
+		if(this.ballMoveState === BallMoveStates.JUMP){
+			this.speed.y += this.gravity * delta;
+		} else if(this.ballMoveState === BallMoveStates.FALL){
+			this.speed.y += this.gravity * delta;
+		} else if(this.ballMoveState === BallMoveStates.STAND){
+			this.speed.y = -PLAYER_VERTICAL_SPEED;
+			this.ballMoveState = BallMoveStates.JUMP;
+		}
+	}
+	updateHorizontalMove(delta: number){
+		const keyInputComponent = this.scene.findGlobalComponentByName<ECS.KeyInputComponent>(ECS.KeyInputComponent.name);
+		const bbox = this.owner.getBounds();
+		if(keyInputComponent.isKeyPressed(ECS.Keys.KEY_LEFT)){
+			this.speed.x = PlayerActions.LEFT * Math.min(PLAYER_HORIZONTAL_SPEED * delta, bbox.left);
+		} else if (keyInputComponent.isKeyPressed(ECS.Keys.KEY_RIGHT)) {
+			this.speed.x = PlayerActions.RIGHT * Math.min(PLAYER_HORIZONTAL_SPEED * delta, SCENE_WIDTH - bbox.right);
+		}
+	}
+
+	applyDynamics(delta: number){
+		if (this.speed.x === 0 && this.speed.y === 0) {
+			return;
+		}
+		const bbox = this.owner.getBounds();
+		if(bbox.left >= 0 && bbox.right <= SCENE_WIDTH ){
+			this.owner.position.x += this.speed.x;
+			this.speed.x *= SLIDING;
+			if (Math.abs(this.speed.x) < 0.01) {
+				this.speed.x = 0;
+			}
+		}else if(bbox.left < 0 ){
+			this.owner.position.x = 0 + this.owner.width;
+			this.speed.x = 0;
+		}else if(bbox.right > SCENE_WIDTH ){
+			this.owner.position.x = SCENE_WIDTH - this.owner.width;
+			this.speed.x = 0;
+		}
+
+		if(bbox.bottom > SCENE_HEIGHT){
+			this.speed.y = -PLAYER_VERTICAL_SPEED;
+			this.ballMoveState = BallMoveStates.JUMP;
+		}
+		if(this.owner.position.y + this.speed.y > this.owner.position.y){
+			this.ballMoveState = BallMoveStates.FALL;
+		}
+		this.owner.position.y += this.speed.y;
+	}
+
+	checkCollisions(){
+		const keyInputComponent = this.scene.findGlobalComponentByName<ECS.KeyInputComponent>(ECS.KeyInputComponent.name);
+		if(keyInputComponent.isKeyPressed(ECS.Keys.KEY_Q)){
+    		this.owner.asGraphics().tint = 0xFF0000;
+		}
+		if(keyInputComponent.isKeyPressed(ECS.Keys.KEY_W)){
+    		this.owner.asGraphics().tint = 0x00FF00;
+		}
+		if(keyInputComponent.isKeyPressed(ECS.Keys.KEY_E)){
+    		this.owner.asGraphics().tint = 0x0000FF;
+		}
+	}
+}
 
 // TODO rename your game
 class MyGame {
@@ -38,8 +142,18 @@ class MyGame {
 	}
 
 	load() {
-		// init the scene and run your game
-
+		let scene = this.engine.scene;
+		scene.addGlobalComponent(new ECS.KeyInputComponent());
+		let ball = new ECS.Graphics();
+		ball.addTag(Tags.BALL);
+		ball.beginFill(0xFFFFFF);
+		ball.lineStyle(1, 0x000000);
+		ball.drawCircle(0, 0, 12);
+		ball.endFill();
+		ball.pivot.set(7.5, 7.5);
+		ball.position.set(0.5 * 800 + 15, 0.9 * 600);
+		ball.addComponent(new BallController());
+		scene.stage.addChild(ball);
 	}
 }
 // this will create a new instance as soon as this file is loaded
